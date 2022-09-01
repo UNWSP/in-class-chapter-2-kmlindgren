23d352ba47eab8114c093dd3ec2a370a090f0196
kmlindgren <kmlindgren@students.unwsp.edu>
2022-09-01 18:34:36 -0500

    Update AvgHeight.java

--- AvgHeight.java
+++ AvgHeight.java
@@ -1,8 +1,8 @@
 
 public class AvgHeight {
 	public static void main(String[] args) {
-		int height1 = 63, height2 = 72, height3 = 65, height4 = 64, height5 = 70;
-		int avgHeight = ((height1 + height2 + height3 + height4 + height5) / 5);
+		float height1 = 63, height2 = 72, height3 = 65, height4 = 64, height5 = 70;
+		float avgHeight = ((height1 + height2 + height3 + height4 + height5) / 5);
 		System.out.println("The average height is " + avgHeight + " inches.");
 	}
 }
