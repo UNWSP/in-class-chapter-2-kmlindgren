cfd64c2d27f9f1750e9dcbd1bd0a099d861212a0
kmlindgren <kmlindgren@students.unwsp.edu>
2022-09-01 19:45:21 -0500

    Create scope.java

---
+++ scope.java
@@ -0,0 +1,17 @@
+
+public class scope {
+
+	final int CONSTANT = 7;
+	static int statvar = 8;
+	public int pubvar = 9;
+	
+	public static void main(String[] args) {
+		System.out.println("The static variable is " + statvar);
+		statvar = 100;
+		System.out.println("The static variable is " + statvar);
+		int pubvar = 120;
+		System.out.println("The public variable is " + pubvar);
+
+	}
+
+}
