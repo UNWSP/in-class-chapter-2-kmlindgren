90ac0d10e961050681daf4bc0b65a57443b6b0d6
kmlindgren <kmlindgren@students.unwsp.edu>
2022-09-01 20:28:18 -0500

    Update scope.java

--- scope.java
+++ scope.java
@@ -1,17 +1,19 @@
 
 public class scope {
 
-	final int CONSTANT = 7;
+	static final int CONSTANT = 7;
 	static int statvar = 8;
 	public int pubvar = 9;
 	
 	public static void main(String[] args) {
+		System.out.println("The constant variable is " + CONSTANT);
 		System.out.println("The static variable is " + statvar);
 		statvar = 100;
 		System.out.println("The static variable is " + statvar);
-		int pubvar = 120;
-		System.out.println("The public variable is " + pubvar);
-
+		{
+			int pubvar = 120;
+			System.out.println("The public variable is " + pubvar);
+		}
 	}
 
 }
